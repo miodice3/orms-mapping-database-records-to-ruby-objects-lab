5ce48d782064e06a633531b09942294cd01e87a0
Michael Iodice <miodice3@gmail.com>
2020-10-29 09:15:06 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,18 +2,26 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    #binding.pry
+    student
     # create a new Student object given a row from the database
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+    SELECT *
+    FROM students
+  SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end   #remove name argument from sql query & also first from this, since we want all the rows.
   end
 
-  def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
-  end
   
   def save
     sql = <<-SQL
@@ -40,4 +48,92 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 9
+  SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end   #remove name argument from sql query & also first from this, since we want all the rows.
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade <= 11
+  SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end   #remove name argument from sql query & also first from this, since we want all the rows.
+  end
+
+  
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 10
+    LIMIT 1
+  SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+      #binding.pry
+    end.first   #remove name argument from sql query & also first from this, since we want all the rows.
+  end
+#all_students_in_grade_X
+
+
+
+
+#number is not
+def self.first_X_students_in_grade_10(number)
+  sql = <<-SQL
+  SELECT *
+  FROM students
+  WHERE grade = 10
+  LIMIT ?
+SQL
+  
+  DB[:conn].execute(sql, number).map do |row|
+    self.new_from_db(row)  
+  end   #remove name argument from sql query & also first from this, since we want all the rows.
+end
+
+#name is a column in the students table
+def self.find_by_name(name)
+  sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE name = ?
+    LIMIT 1
+  SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+     # binding.pry
+      self.new_from_db(row)
+    end.first
+end
+
+def self.all_students_in_grade_X(grade)
+  sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = ?
+  SQL
+
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
+end
+
+
+
 end
